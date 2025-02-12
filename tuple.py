018980376f03236680d4577ac269acdaedcb9646
Subah <subahgoes@gmail.com>
2025-02-13 02:13:15 +0600

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,41 @@
+Trio = ("Messi","Neymar","Suarez",)
+print(Trio)
+
+Trio = ("Messi","Neymar","Suarez",)
+print(Trio[0])
+
+Goat = ("Ronaldo",)
+print(type(Goat))
+
+Trio = ("Messi","Neymar","Suarez",)
+print(len(Trio))
+
+# Range of Indexes
+Trio = ("Messi","Neymar","Suarez",)
+print(Trio[0:2])
+
+Trio = ("Messi","Neymar","Suarez",)
+print(Trio[:3])
+
+Trio = ("Messi","Neymar","Suarez",)
+print(Trio[1:])
+
+# Python - Update Tuples
+Trio = ("Messi","Neymar","Suarez",)
+mnr = list(Trio)
+mnr[2] = "Ronaldo"
+Trio = tuple(mnr)
+print(Trio)
+
+Trio = ("Messi","Neymar","Suarez",)
+mnr = list(Trio)
+mnr.append ("Ronaldo")
+Trio = tuple(mnr)
+print(Trio)
+
+Trio = ("Messi","Neymar","Suarez",)
+mnr = list(Trio)
+mnr.remove ("Suarez")
+Trio = tuple(mnr)
+print(Trio)
+
